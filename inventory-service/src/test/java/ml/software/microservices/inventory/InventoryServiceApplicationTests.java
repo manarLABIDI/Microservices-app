644e14b6f5515be5579086dbfeab6fb765650bbd
manarLABIDI <manarabidi4@gmail.com>
2025-02-16 01:53:46 +0100

    implement inventory service

---
+++ inventory-service/src/test/java/ml/software/microservices/inventory/InventoryServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package ml.software.microservices.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
