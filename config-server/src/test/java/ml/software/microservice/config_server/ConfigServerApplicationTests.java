c16346c2dc46463b451b2e62b842c1bc1215649d
manarLABIDI <manarabidi4@gmail.com>
2025-02-19 02:03:40 +0100

    implement config server

---
+++ config-server/src/test/java/ml/software/microservice/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ml.software.microservice.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
