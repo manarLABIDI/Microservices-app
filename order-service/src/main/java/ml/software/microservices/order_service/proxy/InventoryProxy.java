5ee792f412927a140699a77fab6fa327c9a9cb8e
manarLABIDI <manarabidi4@gmail.com>
2025-02-19 02:54:07 +0100

    refactor configuration to config server

--- order-service/src/main/java/ml/software/microservices/order_service/proxy/InventoryProxy.java
+++ order-service/src/main/java/ml/software/microservices/order_service/proxy/InventoryProxy.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-@FeignClient(value = "inventory", url = "${inventory.url}")
+@FeignClient(name = "INVENTORY-SERVICE")
+//@FeignClient(value = "inventory", url = "${inventory.url}")
 public interface InventoryProxy {
 
     @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
